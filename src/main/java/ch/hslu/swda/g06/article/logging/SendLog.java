8110cb65ecf91b599716cb4d62ac055229a04768
Livio Theiler <livio.theiler@jmcsoftwareag.onmicrosoft.com>
2024-04-15 19:32:45 +0200

    Implement builder pattern

--- src/main/java/ch/hslu/swda/g06/article/logging/SendLog.java
+++ src/main/java/ch/hslu/swda/g06/article/logging/SendLog.java
@@ -7,6 +7,7 @@ import ch.hslu.swda.g06.article.model.Article;
 import ch.hslu.swda.g06.article.model.ReOrder;
 import com.google.gson.Gson;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.stereotype.Component;
 
@@ -23,28 +24,44 @@ public class SendLog {
     }
 
     public void sendArticleCreatedLog(Article article, String correlationId) {
-        Action action = new Action(String.format("Article created: '%s'",article), article.getClass().getName(), article.getArticleId());
+        Action action = new Action.ActionBuilder()
+                .setOperation(String.format("Article created: '%s'",article))
+                .setEntityName(article)
+                .setEntityId(article.getArticleId())
+                .build();
         Log log = new Log(action, article.getStoreId());
 
         sendMessage(log, correlationId, LOG_POST);
     }
 
     public void sendArticleUpdatedLog(Article article, String correlationId) {
-        Action action = new Action(String.format("Article: '%s' updated",article), article.getClass().getName(), article.getArticleId());
+        Action action = new Action.ActionBuilder()
+                .setOperation(String.format("Article: '%s' updated",article))
+                .setEntityName(article)
+                .setEntityId(article.getArticleId())
+                .build();
         Log log = new Log(action, article.getStoreId());
 
         sendMessage(log, correlationId, LOG_POST);
     }
 
     public void sendArticleDeletedLog(Article article, String correlationId) {
-        Action action = new Action(String.format("Article: '%s' deleted",article), article.getClass().getName(), article.getArticleId());
+        Action action = new Action.ActionBuilder()
+                .setOperation(String.format("Article: '%s' deleted",article))
+                .setEntityName(article)
+                .setEntityId(article.getArticleId())
+                .build();
         Log log = new Log(action, article.getStoreId());
 
         sendMessage(log, correlationId, LOG_POST);
     }
 
     public void sendReOrderErrorLog(ReOrder reOrder, String message, String correlationId) {
-        Action action = new Action(message, reOrder.getClass().getName(), reOrder.getReOrderId());
+        Action action = new Action.ActionBuilder()
+                .setOperation(message)
+                .setEntityName(reOrder)
+                .setEntityId(reOrder.getReOrderId())
+                .build();
         Log log = new Log(action, reOrder.getStoreId());
 
         sendMessage(log, correlationId, LOG_POST);
@@ -54,6 +71,8 @@ public class SendLog {
         MessageProperties messageProperties = new MessageProperties();
         messageProperties.setCorrelationId(correlationId);
         messageProperties.setContentType(CONTENT_TYPE);
-        MessageReceiverUtils.sendMessage(log, messageProperties, routingKey, gson, amqpTemplate);
+        String messageBodyJson = gson.toJson(log);
+        Message message = new Message(messageBodyJson.getBytes(), messageProperties);
+        amqpTemplate.send(routingKey, message);
     }
 }
--- src/main/java/ch/hslu/swda/g06/article/logging/model/Action.java
+++ src/main/java/ch/hslu/swda/g06/article/logging/model/Action.java
@@ -1,37 +1,51 @@
 package ch.hslu.swda.g06.article.logging.model;
 
 public class Action {
-    private String operation;
-    private String entityName;
-    private String entityId;
+    private final String operation;
+    private final String entityName;
+    private final String entityId;
 
-    public Action(String operation, String entityName, String entityId) {
-        this.operation = operation;
-        this.entityName = entityName;
-        this.entityId = entityId;
+    private Action(ActionBuilder actionBuilder) {
+        this.operation = actionBuilder.operation;
+        this.entityName = actionBuilder.entityName;
+        this.entityId = actionBuilder.entityId;
     }
 
     public String getOperation() {
         return operation;
     }
 
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
-
     public String getEntityName() {
         return entityName;
-    }
-
-    public void setEntityName(String entityName) {
-        this.entityName = entityName;
     }
 
     public String getEntityId() {
         return entityId;
     }
 
-    public void setEntityId(String entityId) {
-        this.entityId = entityId;
+    public static class ActionBuilder {
+        private String operation;
+        private String entityName;
+        private String entityId;
+
+
+        public ActionBuilder setOperation(String operation) {
+            this.operation = operation;
+            return this;
+        }
+
+        public ActionBuilder setEntityName(Object entity) {
+            this.entityName = entity.getClass().getName();
+            return this;
+        }
+
+        public ActionBuilder setEntityId(String entityId) {
+            this.entityId = entityId;
+            return this;
+        }
+
+        public Action build(){
+            return new Action(this);
+        }
     }
 }
